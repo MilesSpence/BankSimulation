79cc04c7f5e929928138bd79e52945a256f167c8
Miles Spence <mspence@mail.umw.edu>
2021-04-22 18:31:15 -0400

    commit

--- Customer.cpp
+++ Customer.cpp
@@ -1,8 +1,7 @@
 /*  Name Miles Spence
-
-    Pledge: I hereby declare upon my word of honor that I have neither given nor received unauthorized help on this work.
-
-    Program Description: This is the cpp file for the Customer header file. It creates the constructors, data structures, and actually defines what each function/method does. The functions are made up of code which are algorithms and variables to solve problems. A Customer is a name, arrival time, and time to be serviced. In addition, there are getter and setters for all of those parameters.
+    Program Description: This is the cpp file for the Customer header file. It creates the constructors, data structures, and actually defines what each function/method does.
+    The functions are made up of code which are algorithms and variables to solve problems.
+    A Customer is a name, arrival time, and time to be serviced. In addition, there are getter and setters for all of those parameters.
 */
 #ifndef CUSTOMER_CPP
 #define CUSTOMER_CPP
@@ -30,7 +29,6 @@ Customer::Customer(string line) {
     line.append(" ");
     name = line.substr(0, line.find(" "));
     line.erase(line.begin(), line.begin() + line.find(" ")+1);
-    //cout << line << endl;
     
     //get arrival
     string s = line.substr(0, line.find(" "));
@@ -38,26 +36,18 @@ Customer::Customer(string line) {
     string minutes = s;
     string hour = s;
     int aTime;
-    //cout << s.length() << endl;
     if(s.length() == 3) {
         minutes.erase(minutes.begin(), minutes.begin() + 1);
-        //cout << "minutes: " << minutes << endl;
         hour.erase(hour.begin() + 1, hour.end());
-        //cout << "hour: " << hour << endl;
         aTime = atoi(minutes.c_str());
         int hours = atoi(hour.c_str());
         aTime += (hours*60);
-        //cout << "aTime: " << aTime << endl;
     } else {
         minutes.erase(minutes.begin(), minutes.begin() + 2);
-        //cout << "minutes: " << minutes << endl;
         hour.erase(hour.begin() + 2, hour.end());
-        //cout << "hour: " << hour << endl;
         aTime = atoi(minutes.c_str());
         int hours = atoi(hour.c_str());
         aTime += (hours*60);
-        //cout << "aTime: " << aTime << endl;
-
     }
     arrival = aTime;
     
@@ -65,8 +55,6 @@ Customer::Customer(string line) {
     line.erase(line.begin(), line.begin() + line.find(" ")+1);
     s = line.substr(0, line.find(" "));
     service = atoi(s.c_str());
-    //arrival = 0;
-    //service = 0;
 }
 void Customer::SetName(string customerName) {
     name = customerName;
